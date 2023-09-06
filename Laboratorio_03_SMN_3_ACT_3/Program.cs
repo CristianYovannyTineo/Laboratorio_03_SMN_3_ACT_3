23227cc239179bc0c56d6d2b23fe7924d9287161
USUARIO <USUARIO@LAPTOP-BUKF8MRB>
2023-09-05 23:56:33 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio_03_SMN_3_ACT_3/Program.cs
@@ -0,0 +1,49 @@
+﻿
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese la edad : ");
+        int edad = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese adivinanzas acertadas (0-5): ");
+        int adivinanzas = int.Parse(Console.ReadLine());
+
+        double montoInicial = CalcularMontoInicial(edad, adivinanzas);
+        double montoExtra = MontoExtra(edad, adivinanzas);
+        double montoFinal = montoInicial + montoExtra;
+
+        Console.WriteLine($"El monto Total del premio es: S/ {montoFinal:F2}");
+    }
+    static double CalcularMontoInicial(int edad, int adivinanzas)
+    {
+        if (edad >= 18)
+        {
+            if (adivinanzas >= 3)
+            {
+                return 50.0;
+            }
+            else
+            {
+                return 30.0;
+            }
+        }
+        else
+        {
+            if (adivinanzas >= 3)
+            {
+                return 80.0;
+            }
+            else
+            {
+                return 50.0;
+            }
+        }
+    }
+    static double MontoExtra(int edad, int adivinanzas)
+    {
+        double montoExtra = (edad * 5.0) + (adivinanzas * 2.0);
+        return montoExtra;
+    }
+}
